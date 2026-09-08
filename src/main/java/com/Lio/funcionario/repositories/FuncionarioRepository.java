87219290a9040c5526b5c87cefa9cd50e1f3a62c
Lio <liocode.dev@gmail.com>
2026-09-08 11:14:29 -0300

    Delete src directory

--- src/main/java/com/Lio/funcionario/repositories/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Lio.funcionario.repositories;
-
-import com.Lio.funcionario.models.FuncionarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
-}
